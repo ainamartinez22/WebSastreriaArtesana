611c3232d8f47ab59fc72103a5fd5bce6cbfe727
Aina Martínez <ainamartinez22@gmail.com>
2025-12-16 13:33:21 +0100

    Publish in github pages

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import Blog from './views/Blog.vue'
-import Contact from './views/Contact.vue'
 import Gallery from './views/Gallery.vue'
+import Contact from './views/Contact.vue'
 
-
-export default createRouter({
-history: createWebHistory(),
-routes: [
-{ path: '/', component: Home },
-{ path: '/blog', component: Blog },
-{ path: '/galeria', component: Gallery },
-{ path: '/contacto', component: Contact }
-]
+const router = createRouter({
+  history: createWebHashHistory('/WebSastreriaArtesana/'),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/blog', component: Blog },
+    { path: '/galeria', component: Gallery },
+    { path: '/contacto', component: Contact },
+  ]
 })
+
+export default router
+
