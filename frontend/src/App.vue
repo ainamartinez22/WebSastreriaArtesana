a798762e4799a16169c6554dd1889f228ab567d8
Aina Martínez <ainamartinez22@gmail.com>
2025-12-17 11:53:07 +0100

    Inicio+toolbar: mejoras esteticas + logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,45 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Inicio</router-link> 
-      <router-link to="/blog">Blog</router-link> 
-      <router-link to="/galeria">Galería</router-link> 
-      <router-link to="/contacto">Contacto</router-link>
-    </nav>
+    <header class="toolbar">
+      <div class="toolbar-inner">
+        <!-- LOGO -->
+        <router-link to="/" class="logo">
+          MABEL QUIROGA
+        </router-link>
+
+        <!-- BOTÓN HAMBURGER MÓVIL -->
+        <button class="menu-toggle" @click="toggleMenu">
+          ☰
+        </button>
+
+        <!-- NAV -->
+        <nav :class="{ open: menuOpen }" class="nav">
+          <router-link to="/" @click="closeMenu" active-class="active-link">Inicio</router-link>
+          <router-link to="/blog" @click="closeMenu" active-class="active-link">Blog</router-link>
+          <router-link to="/galeria" @click="closeMenu" active-class="active-link">Galería</router-link>
+          <router-link to="/contacto" @click="closeMenu" active-class="active-link">Contacto</router-link>
+        </nav>
+      </div>
+    </header>
 
     <router-view />
   </div>
 </template>
 
 <script setup>
-// App principal
-</script>
+import { ref } from "vue";
 
+const menuOpen = ref(false);
+
+function toggleMenu() {
+  menuOpen.value = !menuOpen.value;
+}
+
+// Cierra el menú al clicar un enlace
+function closeMenu() {
+  menuOpen.value = false;
+}
+</script>
 <style>
-nav {
-  padding: 1rem;
-  background: #222;
-}
-nav a {
-  color: white;
-  margin-right: 1rem;
-  text-decoration: none;
-}
-nav a.router-link-active {
-  font-weight: bold;
-}
+
 </style>
